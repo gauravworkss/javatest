6253fd0801c314a5ac7bf2297282b2b20e4e6a45
Gaurav <saxenagau@gmail.com>
2024-04-03 06:31:31 +0000

    GroupingBy method implementation.

---
+++ src/com/mav/prac/Customer.java
@@ -0,0 +1,3 @@
+package com.mav.prac;
+
+public record Customer(int id, String name,int age, String gender , String department, int yearOfJoining, double sal){}
---
+++ src/com/mav/prac/Product.java
@@ -0,0 +1,32 @@
+package com.mav.prac;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Product {
+    static int id = 1;
+    List<Customer> list = new ArrayList<>();
+    public static void main(String[] args) {
+        Customer c1 = new Customer(id, "test-"+id, id,"Male", "Tax", 2005, 5000d );
+        Customer c2 = new Customer(id++, "test-"+id++, id++,"Male", "Tax", 2005, 5000d );
+        Customer c3 = new Customer(id++, "test-"+id++, id++,"Female", "Tax", 2005, 5000d );
+        Customer c4 = new Customer(id++, "test-"+id++, id++,"Male", "Tax", 2005, 5000d );
+        Customer c5 = new Customer(id++, "test-"+id++, id++,"Female", "Tax", 2005, 5000d );
+
+        Product p = new Product();
+        p.list.add(c1);
+        p.list.add(c2);
+        p.list.add(c3);
+        p.list.add(c4);
+        p.list.add(c5);
+
+        Map<String, List<Customer>> collected = p.list.stream()
+                .collect(Collectors.groupingBy(Customer::gender));
+
+        collected.forEach((gen, size) -> System.out.println(gen+", "+size.size()));
+    }
+
+
+}
